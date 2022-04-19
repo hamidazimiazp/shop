e51d1fbe88934d4e1f2a38451f311b2af4b41345
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-04-19 16:37:53 +0430

    get data in profucts view

--- src/container/Products.jsx
+++ src/container/Products.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadProducts } from '../redux/particles/products';
 
 
 const ProductsView = () => {
+
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.entities.products.list)
+
+    useEffect(() => {
+        dispatch(loadProducts());
+    }, []);
+
+
     return (
         <>
             <h1>Products</h1>
+            <ul>
+                {
+                    products.map(item => {
+                        return <li key={item.id}>{item.title}</li>
+                    })
+                }
+            </ul>
         </>
     );
 }
