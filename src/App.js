780a086f41ceece53b6ac76dc5a1215a590f7045
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-04-19 14:42:54 +0430

    pass store to App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { useEffect } from 'react';
 import configureStore from "./redux/store/store";
 import * as productsActions from "./redux/particles/products";
+import { Provider } from 'react';
 
+
+const store = configureStore();
 
 function App() {
-  const store = configureStore();
-
-
-  store.subscribe(() => {
-    console.log("state change =>", store.getState());
-  });
-
-  store.dispatch(productsActions.loadProducts());
-
-
-  // useEffect(() => {
-  //   console.log(store.getState());
-  // }, [store]);
 
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      Component
+    </Provider>
   );
 }
 
