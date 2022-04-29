7382b864849faa288bde1b6a6de5ea0b3b8d82d4
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-04-29 10:31:57 +0430

    add search box to products page

--- src/container/Products.jsx
+++ src/container/Products.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from '../components/Loading';
 import Product from "../components/Product";
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, TextField } from '@material-ui/core';
 import { loadProducts } from '../redux/particles/products';
 
 
@@ -13,6 +13,15 @@ const ProductsView = () => {
             marginTop: 100,
             justifyContent: "center",
             alignItems: "center"
+        },
+        searchWrapper: {
+            textAlign: "center",
+            "& > div": {
+                width: "50%",
+                [theme.breakpoints.down("sm")]: {
+                    width: "80%"
+                }
+            }
         }
     }));
 
@@ -22,6 +31,23 @@ const ProductsView = () => {
     const dispatch = useDispatch();
     const products = useSelector(state => state.entities.products.list);
     const loading = useSelector(state => state.entities.products.loading);
+    const [search, setSearch] = useState("");
+
+
+    const searchHandler = event => {
+        setSearch(event.target.value);
+    }
+
+    const filtered_products = products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()));
+
+
+    const inputRef = useRef();
+
+    const onKeyHandler = event => {
+        if (event.keyCode === 70) {
+            inputRef.current.children[1].children[0].focus();
+        }
+    }
 
 
     useEffect(() => {
@@ -34,18 +60,24 @@ const ProductsView = () => {
                 loading ?
                     <Loading />
                     :
-                    <Grid container className={classes.container}>
-                        {
-                            products.map(item => {
-                                return <Grid item sm={6} md={4} lg={3} key={item.id}>
-                                    <Product
-                                        id={item.id}
-                                        data={item}
-                                    />
-                                </Grid>
-                            })
-                        }
-                    </Grid>
+                    <>
+                        <Grid container className={classes.container} onKeyDown={onKeyHandler}>
+                            <Grid item xs={12} className={classes.searchWrapper}>
+                                <TextField ref={inputRef} id="outlined-basic" label="Search(F)" variant="outlined" onChange={searchHandler} />
+                            </Grid>
+                            {
+                                filtered_products.map(item => {
+                                    return <Grid item sm={6} md={4} lg={3} key={item.id}>
+                                        <Product
+                                            id={item.id}
+                                            data={item}
+                                        />
+                                    </Grid>
+                                })
+                            }
+                        </Grid>
+                    </>
+
             }
         </>
     );
